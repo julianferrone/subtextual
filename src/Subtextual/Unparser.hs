dd027292c86d0d6602490e9df1a366efa0e21412
jules <81225111+jules@users.noreply.github.com>
2025-07-04 12:56:35 +1000

    Update Unparser to work on BlockOrRef

--- src/Subtextual/Unparser.hs
+++ src/Subtextual/Unparser.hs
@@ -1,29 +1,66 @@
-module Subtextual.Unparser (inline, block, document) where
+module Subtextual.Unparser (unparseInline, unparseBlock, unparseBlockOrRefs) where
 
+import Data.Maybe
 import qualified Data.Text as T
 import Subtextual.Core
 
-inline :: Inline -> T.Text
-inline (PlainText p) = p
-inline (BareUrl url) = url
-inline (AngledUrl url) = T.pack "<" <> url <> T.pack ">"
-inline (SlashLink (DocumentName dn)) = T.pack "/" <> dn
+unparseDocumentName :: DocumentName -> T.Text
+unparseDocumentName (DocumentName docName) = docName
 
-inlines :: [Inline] -> T.Text
-inlines = mconcat . map inline
+unparseInline :: Inline -> T.Text
+unparseInline (PlainText p) = p
+unparseInline (BareUrl url) = url
+unparseInline (AngledUrl url) = T.pack "<" <> url <> T.pack ">"
+unparseInline (SlashLink docName) = T.pack "/" <> unparseDocumentName docName
+
+unparseInlines :: [Inline] -> T.Text
+unparseInlines = mconcat . map unparseInline
 
 spaced :: [T.Text] -> T.Text
 spaced = T.intercalate $ T.pack " "
 
-block :: Block -> T.Text
-block (Paragraph paragraph) = inlines paragraph
-block (Heading heading) = T.pack "# " <> heading
-block (Bullet bullet) = T.pack "- " <> inlines bullet
-block (Quote quote) = T.pack "> " <> inlines quote
-block Blank = T.pack ""
-block (Tag tag) = T.pack "! " <> tag
-block (KeyValue key value) = spaced [T.pack "!", key, value]
-block (Triple subject predicate object) = spaced [T.pack "&", subject, predicate, object]
+----------            Unparsing Blocks            ----------
 
-document :: Document -> T.Text
-document = T.intercalate (T.pack "\n") . map block
+unparseBlock :: Block -> T.Text
+unparseBlock (Paragraph paragraph) = unparseInlines paragraph
+unparseBlock (Heading heading) = T.pack "# " <> heading
+unparseBlock (Bullet bullet) = T.pack "- " <> unparseInlines bullet
+unparseBlock (Quote quote) = T.pack "> " <> unparseInlines quote
+unparseBlock Blank = T.pack ""
+unparseBlock (Tag tag) = T.pack "! " <> tag
+unparseBlock (KeyValue key value) = spaced [T.pack "!", key, value]
+unparseBlock (Triple subject predicate object) = spaced [T.pack "&", subject, predicate, object]
+
+----------         Unparsing Transclusions        ----------
+
+unparseTransclusionOptions :: TransclusionOptions -> [T.Text]
+unparseTransclusionOptions WholeDocument = []
+unparseTransclusionOptions (FirstLines len) =
+  [ T.pack "|",
+    (T.pack . show) len
+  ]
+unparseTransclusionOptions (Lines start len) =
+  [ T.pack "|",
+    (T.pack . show) start,
+    (T.pack . show) len
+  ]
+unparseTransclusionOptions (HeadingSection name) =
+  [ T.pack "#",
+    name
+  ]
+
+unparseTransclusion :: Transclusion -> T.Text
+unparseTransclusion (Transclusion name options) =
+  spaced $
+    [ T.pack "$",
+      unparseDocumentName name
+    ]
+      <> unparseTransclusionOptions options
+
+unparseBlockOrRef :: BlockOrRef -> T.Text
+unparseBlockOrRef = either unparseBlock unparseTransclusion
+
+----------          Unparsing Collections         ----------
+
+unparseBlockOrRefs :: [BlockOrRef] -> T.Text
+unparseBlockOrRefs = T.intercalate (T.pack "\n") . map unparseBlockOrRef
