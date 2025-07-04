0f67ccc7d938ae8b3e22493094ac5e59e9de1439
jules <81225111+jules@users.noreply.github.com>
2025-07-04 13:29:26 +1000

    Update ParserSpec for BlockOrRef parsing

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -11,79 +11,78 @@ shouldMatch parser input result = parseOnly parser input `shouldBe` Right result
 
 spec :: Spec
 spec = do
-  describe "parseNonBlankABlock" $ do
+  describe "parseNonBlankBlock" $ do
     it "parses a paragraph" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "Hello, world!")
-        (Paragraph [PlainText (T.pack "Hello, world!")])
+        $ Left . Paragraph $ [PlainText (T.pack "Hello, world!")]
     it "parses a HTTP URL" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "http://www.google.com")
-        (Paragraph [BareUrl (T.pack "http://www.google.com")])
+        $ Left . Paragraph $ [BareUrl (T.pack "http://www.google.com")]
     it "parses a HTTPS URL" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "https://www.google.com")
-        (Paragraph [BareUrl (T.pack "https://www.google.com")])
+        $ Left . Paragraph $ [BareUrl (T.pack "https://www.google.com")]
     it "parses HTTP and angle-delimited URLs" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "This is a HTTP URL: https://www.google.com, and this is an angle URL: <doi:10.1000/100>")
-        ( Paragraph
+        $ Left . Paragraph $
             [ PlainText (T.pack "This is a HTTP URL: "),
               BareUrl (T.pack "https://www.google.com"),
               PlainText (T.pack ", and this is an angle URL: "),
               AngledUrl (T.pack "doi:10.1000/100")
             ]
-        )
     it "parses a heading" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "# Header")
-        (Heading (T.pack "Header"))
+        $ Left . Heading $ T.pack "Header"
     it "parses a bullet" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "- Bullet")
-        (Bullet [PlainText (T.pack "Bullet")])
+        $ Left . Bullet $ [PlainText (T.pack "Bullet")]
     it "parses a quote" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "> Quote")
-        (Quote [PlainText (T.pack "Quote")])
+        $ Left . Quote $ [PlainText (T.pack "Quote")]
     it "parses a slashlink" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "/foo/bar")
-        (Paragraph [(SlashLink . documentName . T.pack) "foo/bar"])
+        $ Left . Paragraph $ [(SlashLink . documentName . T.pack) "foo/bar"]
     it "parses a tag" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "! Tag")
-        (Tag (T.pack "Tag"))
+        $ Left . Tag $ T.pack "Tag"
     it "parses a key value pair" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "! Key Value")
-        (KeyValue (T.pack "Key") (T.pack "Value"))
+        $ Left $ KeyValue (T.pack "Key") (T.pack "Value")
     it "parses a key value pair with spaces in the value" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "! Key Value 2")
-        (KeyValue (T.pack "Key") (T.pack "Value 2"))
+        $ Left $ KeyValue (T.pack "Key") (T.pack "Value 2")
     it "parses a subject object predicate" $ do
       shouldMatch
-        Parser.parseNonBlankABlock
+        Parser.parseNonBlankBlock
         (T.pack "& Subject Predicate Object")
-        (Triple (T.pack "Subject") (T.pack "Predicate") (T.pack "Object"))
+        $ Left $ Triple (T.pack "Subject") (T.pack "Predicate") (T.pack "Object")
   describe "document" $ do
     it "parses a whole document" $ do
       shouldMatch
         Parser.parseDocument
         (T.pack "# Overview\n\nEvolution is a behavior that emerges in any system with:\n\n- Mutation\n- Heredity\n- Selection\n\nEvolutionary systems often generate unexpected solutions. Nature selects for good enough.\n\n> There is no such thing as advantageous in a general sense. There is only advantageous for the circumstances you\8217re living in. (Olivia Judson, Santa Fe Institute https://overcast.fm/+UtNTAcN2Y/13:36 )\n\nEvolving systems exist in /punctuated-equilibrium.\n\n# Questions\n\n- What systems (beside biology) exhibit evolutionary behavior? Remember, evolution happens in any system with mutation, heredity, selection.\n- What happens to an evolutionary system when you remove mutation? Heredity? Selection?\n- Do you see a system with one of these properties? How can you introduce the other two?\n\n\n# See also\n\nhttps://en.wikipedia.org/wiki/Evolutionary_systems")
-        [ Heading (T.pack "Overview"),
+        (Left <$> [ Heading (T.pack "Overview"),
           Blank,
           Paragraph [PlainText (T.pack "Evolution is a behavior that emerges in any system with:")],
           Blank,
@@ -115,4 +114,4 @@ spec = do
           Heading (T.pack "See also"),
           Blank,
           Paragraph [BareUrl (T.pack "https://en.wikipedia.org/wiki/Evolutionary_systems")]
-        ]
+        ])
