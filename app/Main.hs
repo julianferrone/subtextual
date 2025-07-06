c9ca36ce9fc030f038100da387a30dcf9fc9d122
jules <81225111+jules@users.noreply.github.com>
2025-07-06 14:27:42 +1000

    Remove ConvertFileToHtml command in CLI as we can no longer render just one file--the transclusion references would fail

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import qualified Subtextual.File as F
 import qualified Options.Applicative as A
+import qualified Subtextual.File as F
 import qualified System.FilePath as FP
 
 ------------------------------------------------------------
@@ -10,48 +10,41 @@ import qualified System.FilePath as FP
 
 ----------                Arguments               ----------
 
-newtype Options = Options {optCommand :: Command} deriving Show
+newtype Options = Options {optCommand :: Command} deriving (Show)
 
-data Command = 
-    ConvertFileToHtml FilePath FilePath
-    | ConvertDirectoryToHtml FilePath FilePath
-    deriving Show
+data Command = ConvertDirectoryToHtml FilePath FilePath
+  deriving (Show)
 
 ----------                 Parsing                ----------
 
------- Command Parsing
-
-convertFileToHtml :: A.Parser Command
-convertFileToHtml = 
-    ConvertFileToHtml 
-    <$> A.argument A.str (
-        A.metavar "SUBTEXT_FILEPATH" 
-        <> A.help "The filepath of the Subtext file to read."
-        )
-    <*> A.argument A.str (
-        A.metavar "HTML_FILEPATH" 
-        <> A.help "The filepath to write out the rendered HTML file to."
-        )
-
 convertDirectoryToHtml :: A.Parser Command
-convertDirectoryToHtml = 
-    ConvertDirectoryToHtml
-    <$> A.argument A.str (
-        A.metavar "SUBTEXT_DIR_FILEPATH" 
-        <> A.help "The filepath of the directory of Subtext files to read."
-        )
-    <*> A.argument A.str (
-        A.metavar "HTML_DIR_FILEPATH" 
-        <> A.help "The filepath of the directory to write the rendered HTML files to."
-        )
+convertDirectoryToHtml =
+  ConvertDirectoryToHtml
+    <$> A.argument
+      A.str
+      ( A.metavar "SUBTEXT_DIR_FILEPATH"
+          <> A.help "The filepath of the directory of Subtext files to read."
+      )
+    <*> A.argument
+      A.str
+      ( A.metavar "HTML_DIR_FILEPATH"
+          <> A.help "The filepath of the directory to write the rendered HTML files to."
+      )
 
 ------ Options Parsing
 
 options :: A.Parser Options
-options = Options <$> A.hsubparser
-    ( A.command "file" (A.info convertFileToHtml (A.progDesc "Render a Subtext file into HTML.") )
-    <> A.command "dir" (A.info convertDirectoryToHtml (A.progDesc "Render a directory of Subtext files into HTML.") )
-    )
+options =
+  Options
+    <$> A.hsubparser
+      ( A.command
+          "dir"
+          ( A.info
+              convertDirectoryToHtml
+              ( A.progDesc "Render a directory of Subtext files into HTML."
+              )
+          )
+      )
 
 ----------                Executing               ----------
 
@@ -59,29 +52,31 @@ options = Options <$> A.hsubparser
 
 printConversionMessage :: String -> String -> Either String () -> IO ()
 printConversionMessage src dst (Left err) = do
-    putStrLn $ "Converting Subtext " 
-        <> src 
-        <> " to HTML " 
-        <> dst
-        <> " failed: "
-        <> err
-    return ()
+  putStrLn $
+    "Converting Subtext "
+      <> src
+      <> " to HTML "
+      <> dst
+      <> " failed: "
+      <> err
+  return ()
 printConversionMessage src dst (Right res) = do
-    putStrLn $ "Converting Subtext " 
-        <> src 
-        <> " to HTML "
-        <> dst
-        <> " succeeded."
-    return ()
+  putStrLn $
+    "Converting Subtext "
+      <> src
+      <> " to HTML "
+      <> dst
+      <> " succeeded."
+  return ()
 
 runCommand :: Command -> IO ()
-runCommand (ConvertFileToHtml src dst) = do
-    result <- F.transcribeSubtextToHtml src dst
-    printConversionMessage src dst result
-        
+-- runCommand (ConvertFileToHtml src dst) = do
+--     result <- F.transcribeSubtextToHtml src dst
+--     printConversionMessage src dst result
+
 runCommand (ConvertDirectoryToHtml srcDir dstDir) = do
-    result <- F.transcribeSubtextDirToHtml srcDir dstDir
-    mapM_ (printConversionMessage srcDir dstDir) result
+  result <- F.transcribeSubtextDirToHtml srcDir dstDir
+  mapM_ (printConversionMessage srcDir dstDir) result
 
 ------ Executing Options
 
@@ -94,11 +89,12 @@ runOptions = runCommand . optCommand
 
 main :: IO ()
 main = do
-    opts <- A.execParser 
-        $ A.info 
-        (options A.<**> A.helper) 
-        (A.fullDesc
-        <> A.progDesc "A command-line tool for working with Subtext, the text-based line-oriented hypertext format designed for note-taking."
-        <> A.header "Subtextual CLI"
+  opts <-
+    A.execParser $
+      A.info
+        (options A.<**> A.helper)
+        ( A.fullDesc
+            <> A.progDesc "A command-line tool for working with Subtext, the text-based line-oriented hypertext format designed for note-taking."
+            <> A.header "Subtextual CLI"
         )
-    runOptions opts
+  runOptions opts
